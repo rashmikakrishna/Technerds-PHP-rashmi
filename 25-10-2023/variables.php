6e1139a7d401589634f10eba1cf7139e5909f4aa
rashmikakrishna <rashmikakrishnarashmi964@gmail.com>
2023-10-25 16:44:34 +0530

    first commit

---
+++ 25-10-2023/variables.php
@@ -0,0 +1,15 @@
+<?php
+$name="hello world";
+$age=13;
+$a=5;
+$b=5;
+$type=TRUE;
+echo $name;
+var_dump($name);
+echo "<br>";
+echo $age;
+var_dump($age);
+echo "<br>";
+echo $a+$b;
+var_dump ($a);
+?>
