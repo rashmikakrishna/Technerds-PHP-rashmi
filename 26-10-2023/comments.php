ae7333f27c73035e2aa292719164b412eb773aef
rashmikakrishna <rashmikakrishnarashmi964@gmail.com>
2023-10-26 16:23:03 +0530

    add

---
+++ 26-10-2023/comments.php
@@ -0,0 +1,11 @@
+<?php
+$a="technerds";
+echo $a,"<br>";
+#echo("$a.<br>");
+print $a."<br>";
+print("$a<br>");
+echo("technerds"."<br>".$a); 
+print"technerds"."<br>".$a;
+print("technerds<br>".$a);
+echo"technerds<br>".$a;
+?>
